04de85fcddefcbaedbe9c526f39ad4c2fdd366be
Carlos Marx <carlosmarx27@gmail.com>
2020-08-12 18:38:53 -0300

    Configure react navigation

---
+++ src/pages/GiveClasses/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function GiveClasses() {
+  return (
+    <>
+      <View>
+        <Text>View from Give Classes</Text>
+      </View>
+    </>
+  )
+}
+
+export default GiveClasses
